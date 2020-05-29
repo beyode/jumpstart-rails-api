502b7de00b84ecf37de893167f29477daa6cd738
gathuku <mosesgathuku95@gmail.com>
2020-05-29 21:09:08 +0300

    Add tests

---
+++ test/template_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class TemplateTest < Minitest::Test
+  def setup
+    system('[ -d test_api_app ] && rm -rf test_api_app')
+  end
+
+  def teardown
+    setup
+  end
+
+  def test_generator_succeeds
+    output, _err = capture_subprocess_io do
+      system('DISABLE_SPRING=1 rails new -m template.rb test_api_app --api')
+    end
+    assert_includes output, 'Application generated successfully'
+  end
+end
